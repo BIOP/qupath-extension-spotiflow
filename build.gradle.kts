463386e15b1353b078a8203a78821bdea62ac77c
Rémy Dornier <remy.dornier@epfl.ch>
2025-08-11 13:28:40 +0200

    version 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 qupathExtension {
     name = "qupath-extension-spotiflow"
     group = "ch.epfl.biop"
-    version = "0.2.0-SNAPSHOT"
+    version = "0.2.0"
     description = "QuPath extension to use Spotiflow"
     automaticModule = "qupath.ext.biop.spotiflow"
 }
